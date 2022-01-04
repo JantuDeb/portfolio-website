5a2dae4d35bac590166c5be7ce956167c6b83942
Jantu Deb <63020886+JantuDeb@users.noreply.github.com>
2022-01-04 13:14:41 +0530

    changes some javascript

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const lists = document.querySelector(".project-list")
-
+document.addEventListener('DOMContentLoaded', function () {
+    let footerheight = document.querySelector("footer").offsetHeight;
+    document.querySelector("body").style.paddingBottom = footerheight;
+});
 
 const listsOfProject = [
 
